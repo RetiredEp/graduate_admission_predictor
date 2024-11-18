6c2298f0ebe69751e2101ef5066e99beb36842fc
DODDA VENKATA SRIDHARA REDDY <6zuiidr9@gmail.com>
2024-11-18 22:37:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,6 +26,10 @@ def predict():
     
     return render_template("results.html", prediction=prediction_percentage)
 
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))  # Use PORT if available, else default to 5000
+    app.run(host="0.0.0.0", port=port)
+
 
